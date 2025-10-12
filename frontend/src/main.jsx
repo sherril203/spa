165bde98066024a34959729876e8489a5ef797a1
sherril203 <sherrilkumar2003@gmail.com>
2025-10-12 13:45:20 +0530

    files updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
-import Router from './pages/Router.jsx';
+import Router from './Router.jsx';
 
 
 
