558205e7d884a75aafaa13fb9fbeaf210ec65dad
sherril203 <sherrilkumar2003@gmail.com>
2025-11-24 12:15:39 +0530

    files

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,6 @@ const server=require('./app') //import app.js
 require('dotenv').config()
 const mongodb_uri=process.env.MONGODB_URI
 const port=process.env.PORT
-console.log('mongo',mongodb_uri)
 mongoose.connect(mongodb_uri).then(()=>{
   server.listen(port,()=>{
     console.log(`server is running on port ${port}`)
