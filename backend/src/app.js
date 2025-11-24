a099bbc4008a89c6fdf5e5940ce886ad546b38ac
sherril203 <sherrilkumar2003@gmail.com>
2025-11-24 12:07:07 +0530

    files

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,10 +3,10 @@ const http=require('http')
 const router=require('./routes/routes')
 const path=require('path')
 const app=express()
-// const cors=require('cors')
-// app.use(cors({
-//     origin:"http://localhost:5173" //front end localhost
-// }))
+const cors=require('cors')
+app.use(cors({
+   // origin:"http://localhost:5173" //front end localhost
+}))
 app.use(express.json())
 
 app.use(router)
